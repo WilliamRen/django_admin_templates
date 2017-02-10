954f9f51bbf2cfb1565a5ca3c3a4703c82b1b87b
william <william.ren@live.cn>
2017-02-10 12:59:48 +0800

    add   django-mongoengine

---
+++ libs/django_mongoengine/mongo_auth/backends.py
@@ -0,0 +1,17 @@
+from django.contrib import auth
+
+
+class MongoEngineBackend(object):
+    """Authenticate using MongoEngine and mongoengine.django.auth.User.
+    """
+
+    supports_object_permissions = False
+    supports_anonymous_user = False
+    supports_inactive_user = False
+
+    authenticate = auth.backends.ModelBackend.__dict__["authenticate"]
+    get_user = auth.backends.ModelBackend.__dict__["get_user"]
+    try:
+        user_can_authenticate = auth.backends.ModelBackend.__dict__["user_can_authenticate"]
+    except KeyError:
+        pass # it's okay for django < 1.9
