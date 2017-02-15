65f82cb1bb4d69ae20bb4713a3276163d7fd4950
william <william.ren@live.cn>
2017-02-15 08:52:44 +0800

    update

--- libs/django_mongoengine/__init__.py
+++
@@ -1,6 +0,0 @@
-from .document import Document, DynamicDocument, EmbeddedDocument
-from .queryset import QuerySet, QuerySetNoCache
-
-__all__ = ["QuerySet", "QuerySetNoCache", "Document", "DynamicDocument", "EmbeddedDocument"]
-
-default_app_config = 'django_mongoengine.apps.DjangoMongoEngineConfig'
